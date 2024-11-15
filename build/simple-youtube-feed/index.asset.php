7bbcb6c1ab3f0cacbe7328754f5c0c2c1656af8a
James <james.welbes@gmail.com>
2024-11-15 14:01:41 -0600

    fix carousel styling

--- build/simple-youtube-feed/index.asset.php
+++ build/simple-youtube-feed/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '5a37b8d68cc8e7c2e157');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '78d14536b69e9a2d7bfa');
