7b923853f18dcdc261e1be538728458fc8ba886a
James <james.welbes@gmail.com>
2024-12-04 23:40:17 -0600

    add gallery view

--- src/simple-youtube-feed/save.js
+++ src/simple-youtube-feed/save.js
@@ -11,8 +11,9 @@ export default function save({ attributes }) {
             data-selected-playlist={selectedPlaylist}
             data-enable-search={enableSearch ? 'true' : 'false'}
             data-enable-playlist-filter={enablePlaylistFilter ? 'true' : 'false'}
-            data-channel-id={channelId} // Remove the YT_FOR_WP reference
+            data-channel-id={channelId}
             id="youtube-feed-container"
         ></div>
     );
+    
 }
